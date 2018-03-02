ab82bb0bca9b8cc6db066cce3965c9ccfed6e122
tsunhsiang wen <wents@oregonstate.edu>
2018-03-02 14:25:48 -0800

    file for sending email

--- nba_email.py
+++ nba_email.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.Header import Header
 
+# send NBA daily report from email
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("luckymanyaya@gmail.com", "a45235187")
+server.login("programmingemail0930@gmail.com", "pXf-dqL-DBX-v2Z")
+
+# FROM = 'monty@python.com'
+# TO = ['jon@mycompany.com']  # must be a list
+msg = MIMEMultipart('alternative')
+SUBJECT = "NBA daily report!\n"
+msg['Subject'] = Header(SUBJECT)
 
 
-FROM = 'monty@python.com'
-TO = ['jon@mycompany.com']  # must be a list
-
-SUBJECT = "NBA daily report!"
-TEXT = "This message was sent with Python's smtplib."
-
-msg = """
-From: %s
-To: %s
-Subject: %s
-%s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-
-server.sendmail("luckymanyaya@gmail.com", "luckymanyo@gmail.com", msg)
+# msg = """
+# From: {0}
+# To: {1}
+# Subject: {2}
+#
+# """.format(FROM, ", ".join(TO), SUBJECT)
+msg.attach(MIMEText(open('nba_report.txt', 'rb').read()))
+server.sendmail("programmingemail0930@gmail.com", "tsoliangwu0130@gmail.com", msg.as_string())
 server.quit()
