eace11996ebe9968ea2d2939c3ac00860f4487dd
tsunhsiang wen <wents@oregonstate.edu>
2018-03-02 14:45:54 -0800

    Fix conflict

--- nba.py
+++ nba.py
@@ -166,24 +166,24 @@ if __name__ == "__main__":
             f.write(i)
             f.write('\n')
 
-    # send NBA daily report from email
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login("luckymanyaya@gmail.com", "a45235187")
-
-    FROM = 'monty@python.com'
-    TO = ['jon@mycompany.com']  # must be a list
-    msg = MIMEMultipart('alternative')
-    SUBJECT = "NBA daily report!\n"
-    msg['Subject'] = Header(SUBJECT)
-
-
-    # msg = """
-    # From: {0}
-    # To: {1}
-    # Subject: {2}
+    # # send NBA daily report from email
+    # server = smtplib.SMTP('smtp.gmail.com', 587)
+    # server.starttls()
+    # server.login("programmingemail0930@gmail.com", "pXf-dqL-DBX-v2Z")
     #
-    # """.format(FROM, ", ".join(TO), SUBJECT)
-    msg.attach(MIMEText(open('nba_report.txt', 'rb').read()))
-    server.sendmail("luckymanyay@gmail.com", "tsoliangwu0130@gmail.com", msg.as_string())
-    server.quit()
+    # FROM = 'monty@python.com'
+    # TO = ['jon@mycompany.com']  # must be a list
+    # msg = MIMEMultipart('alternative')
+    # SUBJECT = "NBA daily report!\n"
+    # msg['Subject'] = Header(SUBJECT)
+    #
+    #
+    # # msg = """
+    # # From: {0}
+    # # To: {1}
+    # # Subject: {2}
+    # #
+    # # """.format(FROM, ", ".join(TO), SUBJECT)
+    # msg.attach(MIMEText(open('nba_report.txt', 'rb').read()))
+    # server.sendmail("programmingemail0930@gmail.com", "tsoliangwu0130@gmail.com", msg.as_string())
+    # server.quit()
--- nba_email.py
+++ nba_email.py
@@ -6,18 +6,6 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.Header import Header
 
-<<<<<<< HEAD
-# send NBA daily report from email
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("programmingemail0930@gmail.com", "pXf-dqL-DBX-v2Z")
-
-# FROM = 'monty@python.com'
-# TO = ['jon@mycompany.com']  # must be a list
-msg = MIMEMultipart('alternative')
-SUBJECT = "NBA daily report!\n"
-msg['Subject'] = Header(SUBJECT)
-=======
 
 with open('config.json') as config:
     config_data = json.load(config)
@@ -29,10 +17,11 @@ with open('config.json') as config:
 server = smtplib.SMTP(smtp_host, smtp_port)
 server.starttls()
 server.login(email, password)
->>>>>>> d9907004d8ae44214c42827210bcc2b3f9cb6eb9
 
+msg = MIMEMultipart('alternative')
+SUBJECT = "NBA daily report!\n"
+msg['Subject'] = Header(SUBJECT)
 
-<<<<<<< HEAD
 # msg = """
 # From: {0}
 # To: {1}
@@ -41,17 +30,5 @@ server.login(email, password)
 # """.format(FROM, ", ".join(TO), SUBJECT)
 msg.attach(MIMEText(open('nba_report.txt', 'rb').read()))
 server.sendmail("programmingemail0930@gmail.com", "tsoliangwu0130@gmail.com", msg.as_string())
-=======
-SUBJECT = "NBA daily report!"
-TEXT = "This message was sent with Python's smtplib."
 
-msg = """
-From: {}
-To: {}
-Subject: {}
-{}
-""".format(FROM, ", ".join(TO), SUBJECT, TEXT)
-
-server.sendmail(email, "luckymanyo@gmail.com", msg)
->>>>>>> d9907004d8ae44214c42827210bcc2b3f9cb6eb9
 server.quit()
