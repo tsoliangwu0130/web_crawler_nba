02dc79628e3ee117d2f1444a7123d26eb331b66f
tsunhsiang wen <wents@oregonstate.edu>
2018-03-03 01:04:47 -0800

    format arrangement

--- nba_email.py
+++ nba_email.py
@@ -7,22 +7,22 @@ from email.mime.multipart import MIMEMultipart
 from email.Header import Header
 
 
-with open('config.json') as config:
-    config_data = json.load(config)
-    smtp_host = config_data['smtp_host']
-    smtp_port = config_data['smtp_port']
-    email = config_data['email']
-    password = config_data['password']
+def send_email(subject, filename):
+    with open('config.json') as config:
+        config_data = json.load(config)
+        smtp_host = config_data['smtp_host']
+        smtp_port = config_data['smtp_port']
+        email = config_data['email']
+        password = config_data['password']
 
-server = smtplib.SMTP(smtp_host, smtp_port)
-server.starttls()
-server.login(email, password)
+        server = smtplib.SMTP(smtp_host, smtp_port)
+        server.starttls()
+        server.login(email, password)
 
-msg = MIMEMultipart('alternative')
-SUBJECT = "NBA daily report!\n"
-msg['Subject'] = Header(SUBJECT)
+        msg = MIMEMultipart()
+        msg['Subject'] = Header(subject)
 
-msg.attach(MIMEText(open('nba_report.txt', 'rb').read()))
-server.sendmail("programmingemail0930@gmail.com", "tsoliangwu0130@gmail.com", msg.as_string())
+        msg.attach(MIMEText(open(filename, 'rb').read()))
+        server.sendmail("programmingemail0930@gmail.com", "luckymanyo@gmail.com", msg.as_string())
 
-server.quit()
+        server.quit()
